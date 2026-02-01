2d99c244d4a3bbd9f2b5d702dbe555003758c7bb
prabhat-02-sharma <prabhat.exe02@gmail.com>
2026-02-01 16:08:12 +0530

    Add lib/supabase/server.ts

---
+++ lib/supabase/server.ts
@@ -0,0 +1,28 @@
+import { cookies } from 'next/headers'
+import { createServerClient } from '@supabase/ssr'
+import { type Database } from '@/database.types';
+
+export async function createClient() {
+  const cookieStore = await cookies()
+  const url = process.env.NEXT_PUBLIC_SUPABASE_URL
+  const key = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+  if (!url || !key) return null as any
+
+  return createServerClient<Database>(url, key, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll()
+      },
+      setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options)
+          )
+        } catch {
+          // Safe to ignore in Server Components
+        }
+      },
+    },
+  })
+}
